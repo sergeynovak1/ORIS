4d18115e5d30627d5a9551e7a87bf90d972543fc
Sergey <ssnovak@stud.kpfu.ru>
2022-10-10 14:46:03 +0300

    FLASK DONE!!!

--- Flask/hello.py
+++ Flask/hello.py
@@ -39,29 +39,29 @@ def films_list():
     return render_template("films.html", **context)
 
 
-# метод, который возвращает конкретный фильмо по id по относительному пути /film/<int:film_id>,
-# где film_id - id необходимого фильма
 @app.route("/film/<int:film_id>")
 def get_film(film_id):
-    # читаем файл
     with open("films.json", 'r') as f:
         films = json.load(f)
 
-    # ищем нужный нам фильм и возвращаем шаблон с контекстом
     for film in films:
         if film['id'] == film_id:
             return render_template("film.html", title=film['name'], film=film)
 
-    # если нужный фильм не найден, возвращаем шаблон с ошибкой
     return render_template("error.html", error="Такого фильма не существует в системе")
 
 
-@app.route("/new_film", methods=['post','get', 'delete'])
+@app.route('/new_film', methods=['GET'])
+def my_form():
+    return render_template('new_film.html')
+
+
+@app.route('/new_film', methods=['POST'])
 def new_film():
-    id = request.form.get('id')
-    name = request.form.get('name')
+    id = request.form['id']
+    name = request.form['name']
     rating = request.form.get('rating')
-    country = request.form.get('country')
+    country = request.form['country']
     context = {
         'id': id,
         'name': name,
@@ -70,33 +70,11 @@ def new_film():
     }
     with open("films.json", 'r', encoding='utf-8') as f:
         films = json.load(f)
-    films.append(context)
+        films.append(context)
     with open("films.json", 'w', encoding='utf-8') as f:
         json.dump(films, f)
     return render_template("new_film.html")
 
-@app.route("/films")
-def films_list():
-    # читаем файл с фильмами
-    with open("films.json", 'r') as f:
-        films = json.load(f)
-
-    # получаем GET-параметр country (Russia/USA/French
-    country = request.args.get("country")
-    rating = request.args.get("rating")
-
-    if rating:
-        films = filter(lambda x: x["rating"] >= float(rating), films)
-
-    # формируем контекст, который мы будем передавать для генерации шаблона
-    context = {
-        'films': films,
-        'title': "FILMS",
-        'country': country
-    }
-
-    # возвращаем сгенерированный шаблон с нужным нам контекстом
-    return render_template("films.html", **context)
 
 
 if __name__ == "__main__":
